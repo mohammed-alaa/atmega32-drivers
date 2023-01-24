2fdd23641847b8578d3c387e914c37a48a035fcb
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-01-25 01:27:01 +0200

    Added testing application for GPIO peripheral

---
+++ APPS/TestingGPIO/TestingGPIO_main.h
@@ -0,0 +1,18 @@
+/**
+ * @file TestingGPIO_main.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _TESTINGGPIO_MAIN_H_
+#define _TESTINGGPIO_MAIN_H_
+
+void Init_App(void);
+void Run_App(void);
+
+#endif // _TESTINGGPIO_MAIN_H_
