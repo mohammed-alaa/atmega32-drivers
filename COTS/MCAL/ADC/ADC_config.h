abdbfb0e37d550054ab2aad564080631a2a6b9bd
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-01-27 01:42:18 +0200

    Added ADC module configuration

---
+++ COTS/MCAL/ADC/ADC_config.h
@@ -0,0 +1,42 @@
+/**
+ * @file ADC_config.h
+ * @author Mohamed Alaa
+ * @brief This file contains the configuration information regarding the ADC peripheral
+ * @version 1.0
+ * @date 2023-01-26
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef _ADC_CONFIG_H_
+#define _ADC_CONFIG_H_
+
+#include "ADC_interface.h"
+
+/************************************************************************/
+/*                          Interfacing macros                          */
+/************************************************************************/
+
+/**
+ * @defgroup adc_configuration ADC Configuration
+ * @{
+ */
+
+/**
+ * @def ADC_REF_SELECTION
+ * @brief ADC Voltage Reference Selection
+ * @details This macro is used to select the voltage reference for the ADC
+ * @see adc_voltage_reference_selection
+ */
+#define ADC_REF_SELECTION (ADC_VOLTAGE_REFERENCE_AVCC)
+
+/**
+ * @def ADC_RESULT_ADJUSTMENT
+ * @brief ADC Result Adjustment
+ * @details This macro is used to select the result adjustment direction for the ADC
+ * @see adc_result_adjustment_direction
+ */
+#define ADC_RESULT_ADJUSTMENT (ADC_RESULT_ADJUSTMENT_RIGHT)
+
+/** @} */
+
+#endif // _ADC_CONFIG_H_
