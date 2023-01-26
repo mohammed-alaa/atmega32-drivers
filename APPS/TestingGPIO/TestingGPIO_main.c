4ca1be57a9b8bc105614e252b8a5623b5fbe2d09
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-01-26 14:59:15 +0200

    Updated GPIO testing application

--- APPS/TestingGPIO/TestingGPIO_main.c
+++ APPS/TestingGPIO/TestingGPIO_main.c
@@ -2,7 +2,7 @@
 #include "../../COTS/MCAL/GPIO/GPIO_interface.h"
 #include "TestingGPIO_main.h"
 
-void Init_App(void)
+void vTestingGPIO(void)
 {
     MGPIO_vSetPinDirection(GPIO_PORTB, GPIO_PIN0, INPUT_PULLUP);
     MGPIO_vSetPinDirection(GPIO_PORTB, GPIO_PIN4, INPUT_PULLUP);
@@ -10,34 +10,34 @@ void Init_App(void)
     MGPIO_vSetPinDirection(GPIO_PORTC, GPIO_PIN2, OUTPUT);
     MGPIO_vSetPinDirection(GPIO_PORTC, GPIO_PIN7, OUTPUT);
     MGPIO_vSetPinDirection(GPIO_PORTD, GPIO_PIN3, OUTPUT);
-}
 
-void Run_App(void)
-{
-    if (MGPIO_vGetPinValue(GPIO_PORTB, GPIO_PIN0))
+    while (TRUE)
     {
-        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, HIGH);
-    }
-    else
-    {
-        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, LOW);
-    }
+        if (MGPIO_vGetPinValue(GPIO_PORTB, GPIO_PIN0))
+        {
+            MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, HIGH);
+        }
+        else
+        {
+            MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, LOW);
+        }
 
-    if (MGPIO_vGetPinValue(GPIO_PORTB, GPIO_PIN4))
-    {
-        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, HIGH);
-    }
-    else
-    {
-        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, LOW);
-    }
+        if (MGPIO_vGetPinValue(GPIO_PORTB, GPIO_PIN4))
+        {
+            MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, HIGH);
+        }
+        else
+        {
+            MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, LOW);
+        }
 
-    if (MGPIO_vGetPinValue(GPIO_PORTD, GPIO_PIN2))
-    {
-        MGPIO_vSetPinValue(GPIO_PORTD, GPIO_PIN3, HIGH);
-    }
-    else
-    {
-        MGPIO_vSetPinValue(GPIO_PORTD, GPIO_PIN3, LOW);
+        if (MGPIO_vGetPinValue(GPIO_PORTD, GPIO_PIN2))
+        {
+            MGPIO_vSetPinValue(GPIO_PORTD, GPIO_PIN3, HIGH);
+        }
+        else
+        {
+            MGPIO_vSetPinValue(GPIO_PORTD, GPIO_PIN3, LOW);
+        }
     }
 }
--- APPS/TestingGPIO/TestingGPIO_main.h
+++ APPS/TestingGPIO/TestingGPIO_main.h
@@ -1,6 +1,6 @@
 /**
  * @file TestingGPIO_main.h
- * @author your name (you@domain.com)
+ * @author Mohamed Alaa
  * @brief 
  * @version 0.1
  * @date 2023-01-25
@@ -12,7 +12,6 @@
 #ifndef _TESTINGGPIO_MAIN_H_
 #define _TESTINGGPIO_MAIN_H_
 
-void Init_App(void);
-void Run_App(void);
+void vTestingGPIO(void);
 
 #endif // _TESTINGGPIO_MAIN_H_
