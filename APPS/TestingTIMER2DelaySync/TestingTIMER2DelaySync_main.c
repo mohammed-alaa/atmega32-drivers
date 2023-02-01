0ead3622e9331656c0a92e9195811fe7077c4ad4
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-02-01 22:42:18 +0200

    Added timer 2 delay synchronous testing application

---
+++ APPS/TestingTIMER2DelaySync/TestingTIMER2DelaySync_main.c
@@ -0,0 +1,24 @@
+#include "../../COTS/LIB/LSTD_VALUES.h"
+#include "../../COTS/LIB/LSTD_COMPILER.h"
+#include "../../COTS/MCAL/GPIO/GPIO_interface.h"
+#include "../../COTS/MCAL/TIMER/TIMER_interface.h"
+#include "TestingTIMER2DelaySync_main.h"
+
+void vTestingTIMER2DelaySync(void)
+{
+    MGPIO_vSetPinDirection(GPIO_PORTC, GPIO_PIN2, OUTPUT);
+    MGPIO_vSetPinDirection(GPIO_PORTC, GPIO_PIN7, OUTPUT);
+
+    while (TRUE)
+    {
+        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, HIGH);
+        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, LOW);
+        MTIMER_vSyncDelayMS(TIMER_CHANNEL_2, 5000);
+        // MTIMER_vSyncDelayS(TIMER_CHANNEL_2, 5);
+
+        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN2, LOW);
+        MGPIO_vSetPinValue(GPIO_PORTC, GPIO_PIN7, HIGH);
+        MTIMER_vSyncDelayMS(TIMER_CHANNEL_2, 5000);
+        // MTIMER_vSyncDelayS(TIMER_CHANNEL_2, 5);
+    }
+}
---
+++ APPS/TestingTIMER2DelaySync/TestingTIMER2DelaySync_main.h
@@ -0,0 +1,15 @@
+/**
+ * @file TestingTIMER2DelaySync_main.h
+ * @author Mohamed Alaa
+ * @brief
+ * @version 0.1
+ * @date 2023-02-01
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef _TESTINGTIMER2DELAYSYNC_MAIN_H_
+#define _TESTINGTIMER2DELAYSYNC_MAIN_H_
+
+void vTestingTIMER2DelaySync(void);
+
+#endif // _TESTINGTIMER2DELAYSYNC_MAIN_H_
