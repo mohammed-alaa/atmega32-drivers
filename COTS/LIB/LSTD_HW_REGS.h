8575302e7c6cf1b1be7c95835f2caa0be80e257f
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-02-01 03:51:40 +0200

    Added Register scope for 16-bit registers

--- COTS/LIB/LSTD_HW_REGS.h
+++ COTS/LIB/LSTD_HW_REGS.h
@@ -19,10 +19,29 @@
  */
 
 /**
- * @def REGISTER(ADDRESS)
+ * @def REGISTER(SIZE, ADDRESS)
  * @brief Map to a certain register by its address in the memory
+ * @param ADDRESS The address of the register
+ * @param SIZE The size of the register
+ * @return The value of the register
  */
-#define REGISTER(ADDRESS) (*(volatile P2VAR(u8_t)) (ADDRESS))
+#define REGISTER(SIZE, ADDRESS) (*(volatile P2VAR(SIZE))(ADDRESS))
+
+/**
+ * @def REGISTER_U8(ADDRESS)
+ * @brief Map to a certain register by its 8-bit address in the memory (used for 8-bit registers)
+ * @param ADDRESS The address of the register
+ * @return The value of the register
+ */
+#define REGISTER_U8(ADDRESS) REGISTER(u8_t, ADDRESS)
+
+/**
+ * @def REGISTER_U16(ADDRESS)
+ * @brief Map to a certain register by its 16-bit address in the memory (used for 16-bit registers)
+ * @param ADDRESS The address of the register
+ * @return The value of the register
+ */
+#define REGISTER_U16(ADDRESS) REGISTER(u16_t, ADDRESS)
 
 /** @} */
 
