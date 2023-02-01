756761db7031f5f5be7f2718cff5afa0bc851d75
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-02-01 22:42:03 +0200

    Added timer1 delay synchronous testing application

---
+++ APPS/TestingTIMER1DelaySync/TestingTIMER1DelaySync_main.h
@@ -0,0 +1,15 @@
+/**
+ * @file TestingTIMER1DelaySync_main.h
+ * @author Mohamed Alaa
+ * @brief
+ * @version 0.1
+ * @date 2023-02-01
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef _TESTINGTIMER1DELAYSYNC_MAIN_H_
+#define _TESTINGTIMER1DELAYSYNC_MAIN_H_
+
+void vTestingTIMER1DelaySync(void);
+
+#endif // _TESTINGTIMER1DELAYSYNC_MAIN_H_
