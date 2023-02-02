74cde56bf45e5974525af32de05db9b167756b3e
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-02-03 01:33:48 +0200

    Updated functions and documentation, code optimization, added normal counting mode

--- COTS/MCAL/TIMER/TIMER_program.c
+++ COTS/MCAL/TIMER/TIMER_program.c
@@ -12,18 +12,52 @@
 #include "../../LIB/LSTD_COMPILER.h"
 #include "../../LIB/LSTD_BITMATH.h"
 #include "../../LIB/LSTD_VALUES.h"
-#include "../../LIB/L_INTERRUPTS.h"
-
 #include "TIMER_private.h"
 #include "TIMER_interface.h"
 
 /**
- * @brief This function is used to start the timer in a synchronous mode (blocking)
+ * @brief This function is used to start the timer in a synchronous mode (blocking) in normal mode
+ * @param[in] u8Channel The Timer channel to be used @see timer_channels
+ * @param[in] u8Prescaler The Timer prescaler to be used @see timer_prescaler
+ */
+STATIC void MTIMER_vStartTimerNormal(u8_t u8Channel, u8_t u8Prescaler)
+{
+    switch (u8Channel)
+    {
+    case TIMER_CHANNEL_0:
+        TCCR0 |= (u8Prescaler);
+
+        while (!GET_BIT(TIFR, TIFR_TOV0))
+            ;
+        SET_BIT(TIFR, TIFR_TOV0);
+        break;
+    case TIMER_CHANNEL_1A:
+    case TIMER_CHANNEL_1B:
+        TCCR1B |= (u8Prescaler);
+
+        while (!GET_BIT(TIFR, TIFR_TOV1))
+            ;
+        SET_BIT(TIFR, TIFR_TOV1);
+        break;
+    case TIMER_CHANNEL_2:
+        TCCR2 |= (u8Prescaler);
+
+        while (!GET_BIT(TIFR, TIFR_TOV2))
+            ;
+        SET_BIT(TIFR, TIFR_TOV2);
+        break;
+    default:
+        break;
+    }
+}
+
+/**
+ * @brief This function is used to start the timer in a synchronous mode (blocking) in CTC mode
  * @param[in] u8Channel The Timer channel to be used @see timer_channels
  * @param[in] u8Prescaler The Timer prescaler to be used @see timer_prescaler
  * @param[in] u32Delay The delay
  */
-STATIC void MTIMER_vStartTimerCTCSync(u8_t u8Channel, u8_t u8Prescaler, u32_t u32Delay)
+STATIC void MTIMER_vStartTimerCTC(u8_t u8Channel, u8_t u8Prescaler, u32_t u32Delay)
 {
     switch (u8Channel)
     {
@@ -32,9 +66,9 @@ STATIC void MTIMER_vStartTimerCTCSync(u8_t u8Channel, u8_t u8Prescaler, u32_t u3
 
         while (u32Delay--)
         {
-            while (!GET_BIT(TIFR, 1))
+            while (!GET_BIT(TIFR, TIFR_OCF0))
                 ;
-            SET_BIT(TIFR, 1);
+            SET_BIT(TIFR, TIFR_OCF0);
         }
         break;
     case TIMER_CHANNEL_1A:
@@ -43,9 +77,9 @@ STATIC void MTIMER_vStartTimerCTCSync(u8_t u8Channel, u8_t u8Prescaler, u32_t u3
 
         while (u32Delay--)
         {
-            while (!GET_BIT(TIFR, 4))
+            while (!GET_BIT(TIFR, TIFR_OCF1A))
                 ;
-            SET_BIT(TIFR, 4);
+            SET_BIT(TIFR, TIFR_OCF1A);
         }
         break;
     case TIMER_CHANNEL_2:
@@ -53,11 +87,36 @@ STATIC void MTIMER_vStartTimerCTCSync(u8_t u8Channel, u8_t u8Prescaler, u32_t u3
 
         while (u32Delay--)
         {
-            while (!GET_BIT(TIFR, 7))
+            while (!GET_BIT(TIFR, TIFR_OCF2))
                 ;
-            SET_BIT(TIFR, 7);
+            SET_BIT(TIFR, TIFR_OCF2);
         }
         break;
+    default:
+        break;
+    }
+}
+
+/**
+ * @brief This function is used to set the initial value of TCNTn register in normal mode
+ * @param[in] u8Channel The Timer channel to be used @see timer_channels
+ * @param[in] u32InitialValue The initial value to be set in the TCNTn register
+ * @note This function should be used only in normal mode
+ * @private Internal function
+ */
+STATIC void MTIMER_vSetInitialNormalModeValue(u8_t u8Channel, u16_t u16InitialValue)
+{
+    switch (u8Channel)
+    {
+    case TIMER_CHANNEL_0:
+        TCNT0 = u16InitialValue;
+        break;
+    case TIMER_CHANNEL_1A:
+    case TIMER_CHANNEL_1B:
+        TCNT1 = u16InitialValue;
+        break;
+    case TIMER_CHANNEL_2:
+        TCNT2 = u16InitialValue;
         break;
     default:
         break;
@@ -67,26 +126,25 @@ STATIC void MTIMER_vStartTimerCTCSync(u8_t u8Channel, u8_t u8Prescaler, u32_t u3
 /**
  * @brief This function is used to set the initial value of OCn register in CTC mode
  * @param[in] u8Channel The Timer channel to be used @see timer_channels
- * @param u32InitialValue The initial value to be set in the OCn register
+ * @param[in] u16InitialValue The initial value to be set in the OCn register
  * @note This function should be used only in CTC mode
  * @private Internal function
  */
-STATIC void MTIMER_vSetInitialCTCModeValue(u8_t u8Channel, u32_t u32InitialValue)
+STATIC void MTIMER_vSetInitialCTCModeValue(u8_t u8Channel, u16_t u16InitialValue)
 {
+    MTIMER_vSetInitialNormalModeValue(u8Channel, 0);
+
     switch (u8Channel)
     {
     case TIMER_CHANNEL_0:
-        TCNT0 = 0;
-        OCR0 = u32InitialValue;
+        OCR0 = u16InitialValue;
         break;
     case TIMER_CHANNEL_1A:
     case TIMER_CHANNEL_1B:
-        TCNT1 = 0;
-        OCR1A = u32InitialValue;
+        OCR1A = u16InitialValue;
         break;
     case TIMER_CHANNEL_2:
-        TCNT2 = 0;
-        OCR2 = u32InitialValue;
+        OCR2 = u16InitialValue;
         break;
     default:
         break;
@@ -99,7 +157,7 @@ STATIC void MTIMER_vSetInitialCTCModeValue(u8_t u8Channel, u32_t u32InitialValue
  * @param[in] u8Mode The Timer mode to be used @see timer_modes
  * @param[in] bIsSync Whether the timer is in synchronous mode or not
  */
-STATIC void MTIMER_vInit(u8_t u8Channel, u8_t u8Mode, bool_t bIsSync)
+STATIC void MTIMER_vInit(u8_t u8Channel, u8_t u8Mode)
 {
     MTIMER_vStopTimer(u8Channel);
 
@@ -107,14 +165,7 @@ STATIC void MTIMER_vInit(u8_t u8Channel, u8_t u8Mode, bool_t bIsSync)
     {
     case TIMER_CHANNEL_0:
         TCCR0 = u8Mode;
-        if (bIsSync)
-        {
-            SET_BIT(TIMSK, 1);
-        }
-        else
-        {
-            CLEAR_BIT(TIMSK, 1);
-        }
+        SET_BIT(TIMSK, TIMSK_OCIE0);
         break;
     case TIMER_CHANNEL_1A:
     case TIMER_CHANNEL_1B:
@@ -123,28 +174,12 @@ STATIC void MTIMER_vInit(u8_t u8Channel, u8_t u8Mode, bool_t bIsSync)
         case TIMER_MODE_NORMAL:
             TCCR1A = 0;
             TCCR1B = 0;
-
-            if (bIsSync)
-            {
-                SET_BIT(TIMSK, 2);
-            }
-            else
-            {
-                CLEAR_BIT(TIMSK, 2);
-            }
+            SET_BIT(TIMSK, TIMSK_TOIE1);
             break;
         case TIMER_MODE_CTC:
             TCCR1A = 0;
             TCCR1B = 0x08;
-
-            if (bIsSync)
-            {
-                SET_BIT(TIMSK, ((u8Channel == TIMER_CHANNEL_1A) ? 4 : 3));
-            }
-            else
-            {
-                CLEAR_BIT(TIMSK, ((u8Channel == TIMER_CHANNEL_1A) ? 4 : 3));
-            }
+            SET_BIT(TIMSK, ((u8Channel == TIMER_CHANNEL_1A) ? TIMSK_OCIE1A : TIMSK_OCIE1B));
             break;
         default:
             break;
@@ -152,14 +187,7 @@ STATIC void MTIMER_vInit(u8_t u8Channel, u8_t u8Mode, bool_t bIsSync)
         break;
     case TIMER_CHANNEL_2:
         TCCR2 = u8Mode;
-        if (bIsSync)
-        {
-            SET_BIT(TIMSK, 7);
-        }
-        else
-        {
-            CLEAR_BIT(TIMSK, 7);
-        }
+        SET_BIT(TIMSK, TIMSK_OCIE2);
         break;
     default:
         break;
@@ -186,67 +214,69 @@ void MTIMER_vStopTimer(u8_t u8Channel)
     }
 }
 
-void MTIMER_vSyncDelayUS(u8_t u8Channel, u32_t u32Delay)
+void MTIMER_vDelayUS(u8_t u8Channel, u32_t u32Delay)
 {
     u8_t L_u8Prescaler = ((u8Channel == TIMER_CHANNEL_2) ? TIMER_PRESCALER_TIMER2_8 : TIMER_PRESCALER_TIMER01_8);
-    MTIMER_vInit(u8Channel, TIMER_MODE_CTC, TRUE);
+    MTIMER_vInit(u8Channel, TIMER_MODE_CTC);
     MTIMER_vSetInitialCTCModeValue(u8Channel, 2);
-    MTIMER_vStartTimerCTCSync(u8Channel, L_u8Prescaler, u32Delay);
+    MTIMER_vStartTimerCTC(u8Channel, L_u8Prescaler, u32Delay);
     MTIMER_vStopTimer(u8Channel);
 }
 
-void MTIMER_vSyncDelayMS(u8_t u8Channel, u32_t u32Delay)
+void MTIMER_vDelayMS(u8_t u8Channel, u32_t u32Delay)
 {
     u8_t L_u8Prescaler = ((u8Channel == TIMER_CHANNEL_2) ? TIMER_PRESCALER_TIMER2_64 : TIMER_PRESCALER_TIMER01_64);
-    MTIMER_vInit(u8Channel, TIMER_MODE_CTC, TRUE);
+    MTIMER_vInit(u8Channel, TIMER_MODE_CTC);
     MTIMER_vSetInitialCTCModeValue(u8Channel, 250);
-    MTIMER_vStartTimerCTCSync(u8Channel, L_u8Prescaler, u32Delay);
+    MTIMER_vStartTimerCTC(u8Channel, L_u8Prescaler, u32Delay);
     MTIMER_vStopTimer(u8Channel);
 }
 
-void MTIMER_vSyncDelayS(u8_t u8Channel, u32_t u32Delay)
+void MTIMER_vDelaySec(u8_t u8Channel, u32_t u32Delay)
 {
     u8_t L_u8Prescaler = ((u8Channel == TIMER_CHANNEL_2) ? TIMER_PRESCALER_TIMER2_1024 : TIMER_PRESCALER_TIMER01_1024);
-    MTIMER_vInit(u8Channel, TIMER_MODE_CTC, TRUE);
+    MTIMER_vInit(u8Channel, TIMER_MODE_CTC);
 
-    if ((u8Channel == TIMER_CHANNEL_1A) || (u8Channel == TIMER_CHANNEL_1B))
+    switch (u8Channel)
     {
-        MTIMER_vSetInitialCTCModeValue(TIMER_CHANNEL_1A, 15625);
-        MTIMER_vStartTimerCTCSync(TIMER_CHANNEL_1A, L_u8Prescaler, u32Delay);
-    }
-    else if ((u8Channel == TIMER_CHANNEL_0) || (u8Channel == TIMER_CHANNEL_2))
-    {
+    case TIMER_CHANNEL_0:
+    case TIMER_CHANNEL_2:
         MTIMER_vSetInitialCTCModeValue(u8Channel, 125);
 
         for (u8_t u8LoopCounter = 0; u8LoopCounter < 125; u8LoopCounter++)
         {
-            MTIMER_vStartTimerCTCSync(u8Channel, L_u8Prescaler, u32Delay);
+            MTIMER_vStartTimerCTC(u8Channel, L_u8Prescaler, u32Delay);
         }
-    }
-    else
-    {
-        /* Do nothing */
+        break;
+    case TIMER_CHANNEL_1A:
+    case TIMER_CHANNEL_1B:
+        MTIMER_vSetInitialCTCModeValue(TIMER_CHANNEL_1A, 15625);
+        MTIMER_vStartTimerCTC(TIMER_CHANNEL_1A, L_u8Prescaler, u32Delay);
+        break;
+    default:
+        break;
     }
 
     MTIMER_vStopTimer(u8Channel);
 }
 
-void MTIMER_vSyncDelay(u8_t u8Channel, u8_t u8Mode, u8_t u8Prescaler, u32_t u32Ticks)
+void MTIMER_vDelay(u8_t u8Channel, u8_t u8Mode, u8_t u8Prescaler, u16_t u16InitalValue, u32_t u32Delay)
 {
-    MTIMER_vInit(u8Channel, u8Mode, TRUE);
-    if (u8Mode == TIMER_MODE_CTC)
+    MTIMER_vInit(u8Channel, u8Mode);
+
+    switch (u8Mode)
     {
-        MTIMER_vSetInitialCTCModeValue(u8Channel, u32Ticks);
-    }
-    else if (u8Mode == TIMER_MODE_NORMAL)
-    {
-        // MTIMER_vSetInitialNormalModeValue(u8Channel, u32Ticks);
-    }
-    else
-    {
-        /* Do nothing */
+    case TIMER_MODE_NORMAL:
+        MTIMER_vSetInitialNormalModeValue(u8Channel, u32Delay);
+        MTIMER_vStartTimerNormal(u8Channel, u8Prescaler);
+        break;
+    case TIMER_MODE_CTC:
+        MTIMER_vSetInitialCTCModeValue(u8Channel, u16InitalValue);
+        MTIMER_vStartTimerCTC(u8Channel, u8Prescaler, u32Delay);
+        break;
+    default:
+        break;
     }
 
-    MTIMER_vStartTimerCTCSync(u8Channel, u8Prescaler, u32Ticks);
     MTIMER_vStopTimer(u8Channel);
 }
